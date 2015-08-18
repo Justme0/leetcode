c64d3467cf581631c2047a55a27383e0023035ea
jiangg <jiangg@mail.ustc.edu.cn>
2015-08-18 22:46:57 +0800

    optimize first-missing-positive-hash.rb

--- first-missing-positive-hash.rb
+++ first-missing-positive-hash.rb
@@ -1,7 +1,7 @@
 # void
 def place(nums)
 	nums.each_with_index do |x, i|
-		while x.between?(1, nums.size) and x != i + 1 and nums[i] != nums[x - 1]
+		while x.between?(1, nums.size) and nums[i] != nums[x - 1]
 			nums[i], nums[x - 1] = nums[x - 1], nums[i]
 			x = nums[i]	# NOTE
 		end
