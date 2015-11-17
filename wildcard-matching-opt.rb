bccc806982ae335ee5da52a8e401f0e6b62e7c61
Justme0 <jiangg2012@hotmail.com>
2015-11-17 23:20:12 +0800

    wildcard match optimize

--- wildcard-matching-opt.rb
+++ wildcard-matching-opt.rb
@@ -1,39 +1,30 @@
-$result = {} # cache
-
-def is_match_dfs(s, p)
-	result = $result[[s, p]]
-	return result if !result.nil?
-
-	fail if s.nil? || p.nil?
-	return $result[[s, p]] = s.empty? if p.empty?
-	if s.empty?
-		return $result[[s, p]] = true if p == "*"
-		return $result[[s, p]] = false if p[0] != "*"
-	end
-
-	if p[0] == "*"
-		(0..s.size).each do |i|
-			return $result[[s, p]] = true if is_match_dfs(s[i..-1], p[1..-1])
-		end
-		return $result[[s, p]] = false
-	else
-		if p[0] == "?" or s[0] == p[0]
-			return $result[[s, p]] = is_match_dfs(s[1..-1], p[1..-1])
-		else
-			return $result[[s, p]] = false
-		end
-	end
-end
-
-# @param {String} s
-# @param {String} p
-# @return {Boolean}
 def is_match(s, p)
-	# some optimization
-	return false if p.delete("*").size > s.size
-	return false if p[-1] != "*" && p[-1] != "?" && p[-1] != s[-1]
-	p = p.split("").chunk{|str| str == "*"}.map{|is_star, arr| is_star ? "*" : arr}.join
-	is_match_dfs(s, p)
+	i = 0
+	j = 0
+	star = -1
+	mark = -1
+	while i < s.length
+		if j < p.length && (p[j] == '?' || p[j] == s[i])
+			i += 1
+			j += 1
+		elsif j < p.length && p[j] == '*'
+			star = j
+			j += 1
+			mark = i
+			# 这一步是关键，匹配s中当前字符与p中‘＊’后面的字符，如果匹配，则在第一个if中处理，如果不匹配，则继续比较s中的下一个字符。
+		elsif star != -1
+			j = star + 1
+			mark += 1
+			i = mark
+		else
+			return false
+		end
+	end
+	# 最后在此处处理多余的‘＊’，因为多个‘＊’和一个‘＊’意义相同。
+	while j < p.length && p[j] == '*'
+		j += 1
+	end
+	j == p.length
 end
 
 require "test/unit"
@@ -55,10 +46,10 @@ class TestShit < Test::Unit::TestCase
 		assert is_match("ab", "?*")
 		assert !is_match("aab", "c*a*b")
 		assert !is_match("bbaaaabaaaaabbabbabbabbababaabababaabbabaaabba"\
-				"ababababbabaa"\
-				"bbabbbbbbaaaaaabaabbbbbabbbbabbabababaaaaa",
-				"******aa*bbb*aa*a*bb*ab***bbba*a*babaab*b*aa*a"\
-				"****")
+				 "ababababbabaa"\
+				 "bbabbbbbbaaaaaabaabbbbbabbbbabbabababaaaaa",
+				 "******aa*bbb*aa*a*bb*ab***bbba*a*babaab*b*aa*a"\
+				 "****")
 		assert is_match("abbaaaabbbbbababbbbbbbbaaabaabbabaabbaaabbbbab"\
 				"bbbab",
 				"a*aaba***b**a*a********b")
