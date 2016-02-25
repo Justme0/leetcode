d7f8688a44fbd12626bf2b37b78ee4438dd750df
jiangg <jiangg@mail.ustc.edu.cn>
2016-02-25 21:34:08 +0800

    rename prefix problem number

---
+++ 3-longest-substring-without-repeating-characters.rb
@@ -0,0 +1,11 @@
+=begin
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+=end
+
+# @param {String} s
+# @return {Integer}
+def length_of_longest_substring(s)
+
+end
