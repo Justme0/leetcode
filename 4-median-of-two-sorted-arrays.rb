88d7a11aa3b619d30368fcd502cb09bcb290a5cd
Justme0 <jiangg2012@hotmail.com>
2016-02-28 00:04:44 +0800

    find_median_sorted_arrays: naive algorithm

--- 4-median-of-two-sorted-arrays.rb
+++ 4-median-of-two-sorted-arrays.rb
@@ -6,6 +6,32 @@ There are two sorted arrays nums1 and nums2 of size m and n respectively. Find t
 # @param {Integer[]} nums1
 # @param {Integer[]} nums2
 # @return {Float}
+def find_median_sorted_arrays_trivial(nums1, nums2)
+  arr = (nums1 + nums2).sort
+  if arr.size.odd?
+    arr[(arr.size - 1) / 2]
+  else
+    (arr[arr.size / 2] + arr[arr.size / 2 - 1]) / 2.0
+  end
+end
+
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Float}
 def find_median_sorted_arrays(nums1, nums2)
 
+end
+
+require 'test/unit'
+
+class TestHello < Test::Unit::TestCase
+  def test_hello
+    assert_equal(2, find_median_sorted_arrays([], [1, 2, 3]))
+    assert_equal(2.5, find_median_sorted_arrays([], [2, 3]))
+    assert_equal(1, find_median_sorted_arrays([], [1]))
+    assert_equal(1, find_median_sorted_arrays([1], []))
+    assert_equal(2, find_median_sorted_arrays([1, 2, 3], [1, 2, 3]))
+    assert_equal(2.5, find_median_sorted_arrays([3, 4, 5], [0, 1, 2]))
+    assert_equal(3, find_median_sorted_arrays([3, 4, 5], [1, 2]))
+  end
 end
