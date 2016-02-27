636fa863a6a48c751a5cfc40c8c50f22e4f42bc9
jiangg <jiangg@mail.ustc.edu.cn>
2016-02-27 23:06:19 +0800

    length_of_longest_substring: Accepted

--- 3-longest-substring-without-repeating-characters.rb
+++ 3-longest-substring-without-repeating-characters.rb
@@ -4,8 +4,40 @@ https://leetcode.com/problems/longest-substring-without-repeating-characters/
 Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 =end
 
+require 'test/unit'
+require 'set'
+
 # @param {String} s
 # @return {Integer}
 def length_of_longest_substring(s)
+  first = 0
+  last = 0
+  max_len = 0
+  sub = {} # char => index
+  s.split(//).each_with_index do |c, last|
+    if sub.include?(c)
+      c_index = sub[c] # c is the repeated char
+      (first..c_index).each do |i|
+        sub.delete(s[i])
+      end
+      first = c_index + 1
+      sub[c] = last
+    else
+      sub[c] = last
+      max_len = sub.size if sub.size > max_len
+    end
+  end
+  max_len
+end
 
+class TestHello < Test::Unit::TestCase
+  def test_hello
+    assert_equal(0, length_of_longest_substring(""))
+    assert_equal(3, length_of_longest_substring("abcabcbb"))
+    assert_equal(1, length_of_longest_substring("bbbb"))
+    assert_equal(1, length_of_longest_substring("a"))
+    assert_equal(2, length_of_longest_substring("aabb"))
+    assert_equal(4, length_of_longest_substring("abcd"))
+    assert_equal(2, length_of_longest_substring("aaad"))
+  end
 end
---
+++ 4-median-of-two-sorted-arrays.rb
@@ -0,0 +1,11 @@
+=begin
+https://leetcode.com/problems/median-of-two-sorted-arrays/
+There are two sorted arrays nums1 and nums2 of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+=end
+
+# @param {Integer[]} nums1
+# @param {Integer[]} nums2
+# @return {Float}
+def find_median_sorted_arrays(nums1, nums2)
+
+end
