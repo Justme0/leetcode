e311b6c36ceafb7be4b38a7380844b9fab52d64a
Justme0 <jiangg2012@hotmail.com>
2016-03-01 00:09:21 +0800

    zigzag string: begin

---
+++ 6-zigzag-conversion.rb
@@ -0,0 +1,20 @@
+=begin
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+
+  string convert(string text, int nRows);
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+=end
+
+
+
+# @param {String} s
+# @param {Integer} num_rows
+# @return {String}
+def convert(s, num_rows)
+end
