ceadefa4fb036ff8d0a3ea2db88ff181f3a5a336
jiangg <jiangg@mail.ustc.edu.cn>
2016-03-27 16:34:49 +0800

    Accept

--- 14-longest-common-prefix.cpp
+++ 14-longest-common-prefix.cpp
@@ -6,10 +6,40 @@
 #include <cassert>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 class Solution {
 public:
-  std::string longestCommonPrefix(std::vector<std::string>& strs) {
+  //std::string longestCommonPrefix(std::vector<std::string> &strs) {
+  std::string longestCommonPrefix(std::vector<std::string> strs) {
+    if (strs.empty()) {
+      return "";
+    }
 
+    std::size_t i;
+    for (i = 0; is_valid(strs, i); ++i) {
+    }
+
+    return std::string(strs.front().begin(), strs.front().begin() + i);
+  }
+
+  bool is_valid(const std::vector<std::string> &strs, std::size_t pos) {
+    if (pos >= strs.front().size()) {
+      return false;
+    }
+    char c = strs.front().at(pos);
+    return std::all_of(strs.begin(), strs.end(),
+                       [pos, c](const std::string &s) {
+                         return pos < s.size() && s[pos] == c;
+                       });
   }
 };
+
+int main() {
+  assert("a" == Solution().longestCommonPrefix({"a", "ab"}));
+  std::cout << Solution().longestCommonPrefix({""});
+  std::cout << Solution().longestCommonPrefix({"abb", "ab", "abc"});
+  std::cout << Solution().longestCommonPrefix({""});
+
+  return 0;
+}
