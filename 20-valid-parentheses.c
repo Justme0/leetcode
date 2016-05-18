cef230f2bc70c555bdc01244dcf180a616fc3b1f
jiangg <jiangg@mail.ustc.edu.cn>
2016-05-18 22:44:09 +0800

    Add new problem isValid

---
+++ 20-valid-parentheses.c
@@ -0,0 +1,11 @@
+/*
+   Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+   The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+   */
+
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isValid(char* s) {
+}
