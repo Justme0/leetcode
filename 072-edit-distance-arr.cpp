0731640b8151d17367504b9e8c260d03cf728524
Justme0 <jiangg2012@hotmail.com>
2016-05-21 17:38:33 +0800

    Delete some failed solutions

--- 072-edit-distance-arr.cpp
+++ 072-edit-distance-arr.cpp
@@ -10,18 +10,18 @@ public:
     int len1 = word1.size();
     int len2 = word2.size();
 
-    for (int i = 0; i <= std::max(len1,len2); i++) {
+    for (int i = 0; i <= std::max(len1, len2); i++) {
       dp[i][0] = i;
       dp[0][i] = i;
     }
 
-    for (int i=1; i<=len1; i++) {
-      for (int j=1; j<=len2; j++) {
-        if (word1[i-1]==word2[j-1]) {
-          dp[i][j]=dp[i-1][j-1];//不需操作
+    for (int i = 1; i <= len1; i++) {
+      for (int j = 1; j <= len2; j++) {
+        if (word1[i - 1] == word2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1];
         } else {
-          dp[i][j]=std::min(dp[i][j-1]+1,
-                            std::min(dp[i-1][j]+1,dp[i-1][j-1]+1));
+          dp[i][j] = std::min(dp[i][j - 1] + 1,
+                              std::min(dp[i - 1][j] + 1, dp[i - 1][j - 1] + 1));
         }
       }
     }
@@ -30,8 +30,7 @@ public:
   }
 };
 
-int main()
-{
+int main() {
   assert(0 == Solution().minDistance("", ""));
   assert(1 == Solution().minDistance("a", "ac"));
   assert(0 == Solution().minDistance("a", "a"));
