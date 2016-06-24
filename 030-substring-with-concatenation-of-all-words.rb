a4ffc9695c90245afbdaffca5b56b383696d8589
Justme0 <jiangg2012@hotmail.com>
2016-06-24 13:24:58 +0800

    TODO: substring with all words

--- 030-substring-with-concatenation-of-all-words.rb
+++ 030-substring-with-concatenation-of-all-words.rb
@@ -11,9 +11,40 @@ You should return the indices: [0,9].
 (order does not matter).
 =end
 
+require 'set'
+require 'test/unit'
+
 # @param {String} s
 # @param {String[]} words
 # @return {Integer[]}
 def find_substring(s, words)
+  return [] if words.empty?
+  ret = []
+  len = words[0].length
+  s.length.times do |i|
+    set = words.to_set
+    j = i
+    while !set.empty?
+      word = s[j, len]
+      if set.delete?(word)
+        if set.empty?
+          ret << i
+          next
+        end
+        j += len
+      else
+        # words have no substring
+        i += 1
+      end
+    end
+  end
+  ret
+end
 
+class TestK < Test::Unit::TestCase
+  def test_k
+    assert_equal([0], find_substring("dd", ["dd"]))
+    assert_equal([], find_substring("dd", ["ddddd"]))
+    assert_equal([0, 9], find_substring("barfoothefoobarman", ["foo", "bar"]))
+  end
 end
